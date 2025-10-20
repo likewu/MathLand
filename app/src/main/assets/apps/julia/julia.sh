1c13062c63a4ae788823d77ee5b317ebbf0c754f
likewu <likewu@126.com>
2025-10-20 23:11:59 +0800

    julia1.12.1

--- app/src/main/assets/apps/julia/julia.sh
+++ app/src/main/assets/apps/julia/julia.sh
@@ -7,18 +7,18 @@ if [ ! -f "$HOME"/.local/bin/julia ]; then
    #sudo apt-get update
    cpuarch=$(uname -m)
    cpuarch=${cpuarch:-"aarch64"}
-   wget https://julialang-s3.julialang.org/bin/linux/aarch64/1.11/julia-1.11.5-linux-${cpuarch}.tar.gz
-   tar -xvzf julia-1.11.5-linux-${cpuarch}.tar.gz
+   wget https://julialang-s3.julialang.org/bin/linux/${cpuarch}/1.12/julia-1.12.1-linux-${cpuarch}.tar.gz
+   tar -xvzf julia-1.12.1-linux-${cpuarch}.tar.gz
    #sudo DEBIAN_FRONTEND=noninteractive apt-get -y install r-base r-base-dev 
    if [[ $? != 0 ]]; then
       read -rsp $'An error occurred installing packages, please try again and if it persists provide this log to the developer.\nPress any key to close...\n' -n1 key
    fi
-   rm julia-1.11.5-linux-${cpuarch}.tar.gz
+   rm julia-1.12.1-linux-${cpuarch}.tar.gz
    cd
    mkdir -p .local/bin
    #echo "export PATH=\$HOME/.local/bin:\$PATH" >> ~/.bashrc
    #. ~/.bashrc
-   ln -s "$HOME"/julia-1.11.5/bin/julia "$HOME"/.local/bin
+   ln -s "$HOME"/julia-1.12.1/bin/julia "$HOME"/.local/bin
    #mkdir $HOME/.termux;
    #echo "extra-keys = [['ESC','/','-','$','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','?','LEFT','DOWN','RIGHT','PGDN']]" > $HOME/.termux/termux.properties
 fi
